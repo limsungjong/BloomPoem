0b0300d4d3b7556919840a12074fdc5f2fa470bf
Van <sung8881@naver.com>
2023-02-26 10:01:25 +0900

    remove source files

--- src/main/java/com/example/bloompoem/entity/TestUserEntity.java
+++ src/main/java/com/example/bloompoem/entity/TestUserEntity.java
@@ -5,14 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.sql.Date;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "BP_USER")
+@Table(name = "TEST_USER")
 @NoArgsConstructor
-public class UserEntity {
+public class TestUserEntity {
     @Id
     private String userEmail;
 
