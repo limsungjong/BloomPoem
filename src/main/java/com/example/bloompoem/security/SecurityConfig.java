a52f7e4ab8aca23d399baf6525e57de01770397a
Van <sung8881@naver.com>
2023-02-28 08:13:55 +0900

    test

--- src/main/java/com/example/bloompoem/security/SecurityConfig.java
+++ src/main/java/com/example/bloompoem/security/SecurityConfig.java
@@ -9,32 +9,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/css/**")
-                .antMatchers("/build/**")
-                .antMatchers("/favicon*/**")
-                .antMatchers("/image/**")
-        ;
-    }
-
-    // 디폴트 로그인 페이지를 다른 곳으로 변환
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login")
-                .permitAll();
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring()
+//                .antMatchers("/css/**")
+//                .antMatchers("/build/**")
+//                .antMatchers("/favicon*/**")
+//                .antMatchers("/image/**")
+//        ;
+//    }
+//
+//    // 디폴트 로그인 페이지를 다른 곳으로 변환
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.csrf().disable();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/")
+//                .permitAll()
+//                .anyRequest().authenticated()
+//                .and().formLogin().loginPage("/login")
+//                .permitAll();
 //        http.authorizeRequests().antMatchers("/","/test").permitAll();
 //        http
 //                .cors().and().csrf().disable().authorizeRequests();
 //                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
-    }
+//    }
 
 //    @Bean
 //    public CorsConfigurationSource corsConfiguration () {
