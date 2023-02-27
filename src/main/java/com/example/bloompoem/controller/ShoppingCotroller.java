c69ec7653726c53823556a5bd0844b8ab83bcee2
범수 <delt96@naver.com>
2023-02-27 16:34:50 +0900

    DB변경으로인한 Entity수정

--- src/main/java/com/example/bloompoem/controller/ShoppingCotroller.java
+++ src/main/java/com/example/bloompoem/controller/ShoppingCotroller.java
@@ -3,10 +3,18 @@ package com.example.bloompoem.controller;
 import com.example.bloompoem.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProductCotroller {
+public class ShoppingCotroller {
     @Autowired
     private ProductService productService;
+
+
+    @GetMapping("/shopping")
+    public String shopping (){
+
+        return "/shop/shopMain";
+    }
 
 }
