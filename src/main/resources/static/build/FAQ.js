006a8a3dde8a2c16cf07a253a0b8a9edd0e7fe06
narae <jnr9933@naver.com>
2023-02-28 08:59:57 +0900

    QnA, FAQ 파일 추가

---
+++ src/main/resources/static/build/FAQ.js
@@ -0,0 +1,16 @@
+const qList = document.querySelectorAll("p.FAQ_toggle_Q");
+const aList = document.querySelectorAll("p.FAQ_toggle_A");
+for (let index = 0; index < qList.length; index++) {
+  const question = qList[index];
+
+  console.log(question);
+
+  const answer = aList[index];
+  console.log(answer);
+
+  $(question).click(function () {
+    $(answer).toggle();
+  });
+}
+console.log(qList);
+console.log(aList);
