8e201424308cb34a3527e115a32707f60b8413f4
범수 <delt96@naver.com>
2023-02-28 13:42:39 +0900

    카테고리별 물건 불러오기를 만들었습니다.-미완-(2.28)

--- src/main/java/com/example/bloompoem/controller/ShoppingCotroller.java
+++ src/main/java/com/example/bloompoem/controller/ShoppingCotroller.java
@@ -1,15 +1,21 @@
 package com.example.bloompoem.controller;
 
+import com.example.bloompoem.entity.ProductEntity;
 import com.example.bloompoem.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ShoppingCotroller {
     @Autowired
     private ProductService productService;
-
+    private static Logger logger = LoggerFactory.getLogger(ShoppingCotroller.class);
 
     @GetMapping("/shopping")
     public String shopping (){
@@ -23,5 +29,11 @@ public class ShoppingCotroller {
         return "/shop/shopCategory";
     }
 
+    //카테고리별 물건 불러오기
+    @GetMapping("/productListView")
+    public List<ProductEntity> view (char category) {
+        List<ProductEntity> product = productService.categoryProductView(category);
 
+        return product;
+    }
 }
--- src/main/java/com/example/bloompoem/repository/ProductRepository.java
+++ src/main/java/com/example/bloompoem/repository/ProductRepository.java
@@ -4,6 +4,9 @@ package com.example.bloompoem.repository;
 import com.example.bloompoem.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity,Integer> {
 
+        public List<ProductEntity> findByProductCategory(char category);
 }
