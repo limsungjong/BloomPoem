9861dffe5c5ce2a8e480023bb9132440d1487de9
범수 <delt96@naver.com>
2023-02-28 13:42:39 +0900

    카테고리별 물건 불러오기를 만들었습니다.-미완-(2.28)

--- src/main/java/com/example/bloompoem/service/ProductService.java
+++ src/main/java/com/example/bloompoem/service/ProductService.java
@@ -1,13 +1,28 @@
 package com.example.bloompoem.service;
 
+import com.example.bloompoem.entity.ProductEntity;
 import com.example.bloompoem.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository dao;
+    private static Logger logger = LoggerFactory.getLogger(ProductService.class);
 
+    public List<ProductEntity> categoryProductView (char category){
+        List<ProductEntity> product = null;
 
+        try{
+            product = dao.findByProductCategory(category);
+        }catch(Exception e){
+            logger.error("[ProductService] categoryProductView Exception ", e);
+        }
+        return product;
+    }
 }
