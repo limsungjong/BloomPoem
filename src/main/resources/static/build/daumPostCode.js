222c18350452bc811f3e0ffe1f03391e73072791
Van <sung8881@naver.com>
2023-03-03 16:46:12 +0900

    daumpostcode update

--- src/main/resources/static/build/daumPostCode.js
+++ src/main/resources/static/build/daumPostCode.js
@@ -13,34 +13,4 @@
       },
     }).open();
   });
-  document.querySelector("#submit").addEventListener("click", (e) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Cookie", "JSESSIONID=CAC23876470920D5AB5B67C6F297AAC0");
-
-    const inputData = JSON.stringify({
-      userEmail: userEmail.value,
-      userAddress: addressInput.value,
-      userAddressDetail: userAddressDetail.value,
-      userPhoneNumber: `${p1Num.value}-${p2Num.value}-${p3Num.value}`,
-      userName: document.querySelector("#nameInput").value,
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: inputData,
-      redirect: "follow",
-    };
-
-    const reg = new RegExp(emailTest);
-    if (!reg.test(userEmail.value)) {
-      console.log("error");
-      return;
-    }
-
-    fetch("http://localhost:9000/api/v1/sign/sign_up", requestOptions)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  });
 }
