6216483c950b29780d1b3a1270818e420897f6e8
Van <sung8881@naver.com>
2023-03-05 00:06:59 +0900

    security config update

--- src/main/java/com/example/bloompoem/security/WebSecurityConfigure.java
+++ src/main/java/com/example/bloompoem/security/WebSecurityConfigure.java
@@ -1,41 +1,46 @@
-//package com.example.bloompoem.security;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class WebSecurityConfigure {
-//    private final Logger log = LoggerFactory.getLogger(getClass());
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/","/shopping/**","/api/**","/sign/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/sign/sign_in")
-//                .permitAll();
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers(
-//                "/css/**",
-//                "/build/**",
-//                "/favicon*/**",
-//                "/image/**"
-//                );
-//    }
-//}
+package com.example.bloompoem.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfigure {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/","/shopping/**","/api/**","/sign/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/sign/sign_in")
+                .usernameParameter("userEmail")
+                .defaultSuccessUrl("/")
+                .loginProcessingUrl("/loginProc")
+                .permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers(
+                "/css/**",
+                "/build/**",
+                "/favicon*/**",
+                "/image/**"
+                );
+    }
+}
