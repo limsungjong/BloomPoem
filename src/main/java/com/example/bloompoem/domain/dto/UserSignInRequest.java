03752af7de50c5c8f35e45bcc70a935c6f40e55e
Van <sung8881@naver.com>
2023-03-05 00:09:28 +0900

    add UserSignInRequest

---
+++ src/main/java/com/example/bloompoem/domain/dto/UserSignInRequest.java
@@ -0,0 +1,12 @@
+package com.example.bloompoem.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSignInRequest {
+    private String userEmail;
+
+    private String userOtp;
+}
