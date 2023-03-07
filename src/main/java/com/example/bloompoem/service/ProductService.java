156dbaad8ffff8040f24f9d5bd5b002c79391aa5
범수 <delt96@naver.com>
2023-03-07 19:14:12 +0900

    장바구니에 넣는 기능 추가

--- src/main/java/com/example/bloompoem/service/ProductService.java
+++ src/main/java/com/example/bloompoem/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.example.bloompoem.service;
 
+
 import com.example.bloompoem.entity.ProductEntity;
+import com.example.bloompoem.entity.ShoppingCartEntity;
 import com.example.bloompoem.repository.ProductRepository;
+import com.example.bloompoem.repository.ShoppingCartRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +17,16 @@ import java.util.List;
 @Service
 public class ProductService {
     @Autowired
-    private ProductRepository dao;
+    private ProductRepository productDao;
+    @Autowired
+    private ShoppingCartRepository cartDao;
     private static Logger logger = LoggerFactory.getLogger(ProductService.class);
 
     public Page<ProductEntity> categoryProductView (int category ,Pageable pageable){
         Page<ProductEntity> product = null;
 
         try{
-            product = dao.findAllByProductCategory(category, pageable);
+            product = productDao.findAllByProductCategory(category, pageable);
         }catch(Exception e){
             logger.error("[ProductService] categoryProductView Exception ", e);
         }
@@ -30,21 +35,34 @@ public class ProductService {
     public Page<ProductEntity> productView(Pageable pageable){
         Page<ProductEntity> product=null;
         try{
-            product = dao.findAll(pageable);
+            product = productDao.findAll(pageable);
         }catch(Exception e){
             logger.error("[ProductService] ProductView Exception ", e);
         }
         return product;
     }
     public ProductEntity viewOne (int productNumber){
-        ProductEntity product = dao.findByProductNumber(productNumber);
+        ProductEntity product = productDao.findByProductNumber(productNumber);
         return product;
     }
     public Page<ProductEntity> searchProduct(String searchValue, Pageable pageable){
-        Page<ProductEntity> product = dao.findByProductNameContaining(searchValue, pageable);
+        Page<ProductEntity> product = productDao.findByProductNameContaining(searchValue, pageable);
         if(product == null){
             product= null;
         }
         return product;
     }
+    public void saveCart(ShoppingCartEntity cart){
+        cartDao.saveAndFlush(cart);
+    }
+    public boolean existsProduct(int productNumber, String userEmail){
+        return cartDao.existsByProductNumberAndUserEmail(productNumber, userEmail);
+    }
+    public int countCart(String userEmail){
+        return cartDao.countByUserEmail(userEmail);
+    }
+    public List<ShoppingCartEntity> viewCart(String userEmail){
+        return cartDao.findByUserEmail(userEmail);
+    }
+
 }
