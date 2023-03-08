22a8956129e302b25c3b7b573f1e2e331b25adbd
Van <sung8881@naver.com>
2023-03-09 08:59:24 +0900

    update FlowerEntity FloristFlowerInterFace FloristRepository

--- src/main/java/com/example/bloompoem/entity/FlowerEntity.java
+++ src/main/java/com/example/bloompoem/entity/FlowerEntity.java
@@ -1,9 +1,6 @@
 package com.example.bloompoem.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -12,6 +9,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 @SequenceGenerator(
         name = "SEQ_FLOWER_NUMBER",
