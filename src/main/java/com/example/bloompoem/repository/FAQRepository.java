c002185ead904ffe88b9bf361b2c79a27e883144
narae <jnr9933@naver.com>
2023-03-10 14:42:11 +0900

    QnA.js & QnAwrite.js ajax 주석처리, html쪽 form태그 수정, controller 수정

--- src/main/java/com/example/bloompoem/repository/FAQRepository.java
+++ src/main/java/com/example/bloompoem/repository/FAQRepository.java
@@ -2,7 +2,9 @@ package com.example.bloompoem.repository;
 
 import com.example.bloompoem.entity.FAQEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FAQRepository extends JpaRepository<FAQEntity, Integer> {
     //@Query(value = "SELECT *   FROM FAQ   WHERE FAQ_NUMBER = faqNumber", nativeQuery = true);
 }
--- src/main/java/com/example/bloompoem/repository/QnARepository.java
+++ src/main/java/com/example/bloompoem/repository/QnARepository.java
@@ -2,6 +2,8 @@ package com.example.bloompoem.repository;
 
 import com.example.bloompoem.entity.QnAEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QnARepository extends JpaRepository<QnAEntity, Integer> {
 }
