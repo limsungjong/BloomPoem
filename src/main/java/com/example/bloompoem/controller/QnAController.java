2f73c832aa4aa8e1ed25e66bdb545276c94eef96
Van <sung8881@naver.com>
2023-03-10 15:15:02 +0900

    no

--- src/main/java/com/example/bloompoem/controller/QnAController.java
+++ src/main/java/com/example/bloompoem/controller/QnAController.java
@@ -1,5 +1,6 @@
 package com.example.bloompoem.controller;
 
+import com.example.bloompoem.dto.QnADTO;
 import com.example.bloompoem.entity.QnAEntity;
 import com.example.bloompoem.service.QnAService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class QnAController {
@@ -17,16 +18,25 @@ public class QnAController {
     // 나의 문의 내역 리스트
     @GetMapping("/QnA")
     public String getQnAList(Model model, Pageable pageable){
-        Page<QnAEntity> qnAEntityPage = qnaService.getQnAList(pageable);
-        model.addAttribute("QnAList", qnAEntityPage);
+        Page<QnAEntity> qnaEntityPage = qnaService.getQnAList(pageable);
+        model.addAttribute("QnAList", qnaEntityPage);
         return "/QnA";
     }
 
     // 문의 글쓰기
     @GetMapping("/QnA/write")
     public String write(Model model){
-        return "/QnA/write";
+        return "/QnAwrite";
     }
+
+    @PostMapping("/QnA/write")
+    public String write(@ModelAttribute QnADTO qnaDTO){
+        qnaService.write(qnaDTO);
+
+        return "/QnAwrite";
+    }
+
+    //
 
 
     // 문의 글 보기
