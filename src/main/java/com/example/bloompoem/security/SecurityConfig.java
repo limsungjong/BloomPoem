ddc65a79fe8c2e115538a7895a158ad83e481f28
Van <sung8881@naver.com>
2023-03-10 16:12:03 +0900

    SecurityConfig

--- src/main/java/com/example/bloompoem/security/SecurityConfig.java
+++ src/main/java/com/example/bloompoem/security/SecurityConfig.java
@@ -1,36 +1,14 @@
-//package com.example.bloompoem.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring()
-//                .antMatchers("/css/**")
-//                .antMatchers("/build/**")
-//                .antMatchers("/favicon*/**")
-//                .antMatchers("/image/**")
-//        ;
-//    }
-//
-//    // 디폴트 로그인 페이지를 다른 곳으로 변환
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception{
-//        http.csrf().disable();
-//        http
-//                .authorizeRequests()
-////                .anyRequest().authenticated()
-//                .antMatchers("/","/shopping/**","/shopCategory","/api/**","/sign/**")
-//                .permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/signIn")
-//                .permitAll();
-//    }
-//
-//}
+package com.example.bloompoem.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
