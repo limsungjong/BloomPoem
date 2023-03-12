78d5404a271ffa4c575c5ad4b40ffff49c241446
Van <sung8881@naver.com>
2023-03-12 09:57:15 +0900

    update related PickUpCart

--- src/main/java/com/example/bloompoem/repository/PickUpCartRepository.java
+++ src/main/java/com/example/bloompoem/repository/PickUpCartRepository.java
@@ -4,6 +4,16 @@ import com.example.bloompoem.entity.PickUpCartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface PickUpCartRepository extends JpaRepository<PickUpCartEntity, Long> {
+public interface PickUpCartRepository extends JpaRepository<PickUpCartEntity, Integer> {
+    List<PickUpCartEntity> findPickUpCartEntitiesByUserEmail(String userEmail);
+    Optional<PickUpCartEntity> findByUserEmailAndFlowerNumber(String userEmail, int flowerNumber);
+
+    List<PickUpCartEntity> findByUserEmail(String userEmail);
+
+    Optional<PickUpCartEntity> findByUserEmailAndFlowerNumberAndFloristNumber(String userEmail, int flowerNumber, int floristNumber);
+
 }
