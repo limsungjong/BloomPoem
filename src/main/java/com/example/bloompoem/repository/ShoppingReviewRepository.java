7ba02c95c2233f7d5618c4a605b647b05bf25503
범수 <delt96@naver.com>
2023-03-15 09:09:15 +0900

    review관련 entity 작성 및 repository 작성

---
+++ src/main/java/com/example/bloompoem/repository/ShoppingReviewRepository.java
@@ -0,0 +1,8 @@
+package com.example.bloompoem.repository;
+
+import com.example.bloompoem.entity.ShoppingReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingReviewRepository extends JpaRepository<ShoppingReview, Integer> {
+
+}
