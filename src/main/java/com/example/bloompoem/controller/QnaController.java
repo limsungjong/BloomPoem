ccaa5a9b7d42d27e3adbef43e88ce74bcd30b31a
narae <jnr9933@naver.com>
2023-03-16 16:41:47 +0900

    작성버튼 더블클릭 고침

--- src/main/java/com/example/bloompoem/controller/QnaController.java
+++ src/main/java/com/example/bloompoem/controller/QnaController.java
@@ -1,6 +1,5 @@
 package com.example.bloompoem.controller;
 
-import com.example.bloompoem.dto.QnaDTO;
 import com.example.bloompoem.entity.QnaEntity;
 import com.example.bloompoem.repository.QnaRepository;
 import com.example.bloompoem.service.QnaService;
@@ -8,16 +7,13 @@ import com.example.bloompoem.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,14 +28,6 @@ public class QnaController {
     // 나의 문의 내역 리스트
     @GetMapping("/qna")
     public String getQnaList(Model model, Pageable pageable){
-        Page<QnaEntity> qnaEntityPage = qnaService.getQnaList(pageable);
-        model.addAttribute("QnaList", qnaEntityPage);
-        return "/qna";
-    }
-
-    @PostMapping("/qna")
-    public String backToQnaList(Model model, Pageable pageable){
-        // 다시 기존 페이지로 돌아가는데 데이터가 안넘어감
         Page<QnaEntity> qnaEntityPage = qnaService.getQnaList(pageable);
         model.addAttribute("QnaList", qnaEntityPage);
         return "/qna";
--- src/main/java/com/example/bloompoem/service/QnaService.java
+++ src/main/java/com/example/bloompoem/service/QnaService.java
@@ -1,12 +1,10 @@
 package com.example.bloompoem.service;
 
-import com.example.bloompoem.dto.QnaDTO;
 import com.example.bloompoem.entity.QnaEntity;
 import com.example.bloompoem.repository.QnaRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
