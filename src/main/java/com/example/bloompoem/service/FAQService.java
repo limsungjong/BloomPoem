a2849d3e02b023ed6d704d6fe4a4bf9392fdaa11
narae <jnr9933@naver.com>
2023-03-21 10:09:21 +0900

    Qna 페이지 로그인 처리

--- src/main/java/com/example/bloompoem/service/FAQService.java
+++ src/main/java/com/example/bloompoem/service/FAQService.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 @RequiredArgsConstructor
