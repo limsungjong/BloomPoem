5eb28560c8bfb075cd09f7d18b30cc169b285ec8
범수 <delt96@naver.com>
2023-03-26 20:52:17 +0900

    faq, qna 모두 수정

--- src/main/java/com/example/bloompoem/entity/QnaEntity.java
+++ src/main/java/com/example/bloompoem/entity/QnaEntity.java
@@ -1,10 +1,7 @@
 package com.example.bloompoem.entity;
 
 import com.example.bloompoem.dto.QnaDTO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -12,12 +9,10 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @DynamicUpdate
-@Entity
+@Entity(name = "qna")
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@Table(name = "QNA")
+@Data
 @SequenceGenerator(
         name = "SEQ_QNA_NUMBER",
         sequenceName = "SEQ_QNA_NUMBER",
@@ -29,69 +24,16 @@ public class QnaEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_QNA_NUMBER")
     private Integer qnaNumber;
 
-    @JoinColumn(name = "userEmail")
     private String userEmail;
-
-    @CreationTimestamp
-    @Column
     private LocalDateTime qnaDate;
-
-    @Column
     private char qnaStatus;
-
-    @Column(length = 100)
     private String qnaTitle;
-
-    @Column
     private int qnaGroup;
-
-    @Column
     private int qnaIndent;
-
-    @Column
     private int qnaParent;
-
-    @Column
     private int qnaOrder;
-
-    @Column(length = 50)
     private String qnaImage1;
-
-    @Column(length = 50)
-    private String qnaImage2;
-
-    @Column(length = 50)
-    private String qnaImage3;
-
-    @Column(length = 500)
     private String qnaContent;
 
-    // 1:1 문의 목록에 띄울 문의번호, 상태, 제목, 등록일만 가져오기
-    public static QnaEntity getQnaListToEntity(QnaDTO qnaDTO){
-        QnaEntity qnaEntity = new QnaEntity();
-        qnaEntity.setQnaNumber(qnaDTO.getQnaNumber());
-        qnaEntity.setQnaStatus(qnaDTO.getQnaStatus());
-        qnaEntity.setQnaTitle(qnaDTO.getQnaTitle());
-        qnaEntity.setQnaDate(qnaDTO.getQnaDate());
-        return qnaEntity;
-    }
 
-    // Qna 작성에 필요한 정보 가져오기(일단 다 넣었음)
-    public static QnaEntity toEntity(QnaDTO qnaDTO){
-        QnaEntity qnaEntity = new QnaEntity();
-        qnaEntity.setQnaNumber(qnaDTO.getQnaNumber());
-        qnaEntity.setUserEmail(qnaDTO.getUserEmail());
-        qnaEntity.setQnaDate(qnaDTO.getQnaDate());
-        qnaEntity.setQnaStatus(qnaDTO.getQnaStatus());
-        qnaEntity.setQnaTitle(qnaDTO.getQnaTitle());
-        qnaEntity.setQnaGroup(qnaDTO.getQnaGroup());
-        qnaEntity.setQnaIndent(qnaDTO.getQnaIndent());
-        qnaEntity.setQnaParent(qnaDTO.getQnaParent());
-        qnaEntity.setQnaOrder(qnaDTO.getQnaOrder());
-        qnaEntity.setQnaImage1(qnaDTO.getQnaImage1());
-        qnaEntity.setQnaImage2(qnaDTO.getQnaImage2());
-        qnaEntity.setQnaImage3(qnaDTO.getQnaImage3());
-        qnaEntity.setQnaContent(qnaDTO.getQnaContent());
-        return toEntity(qnaDTO);
-    }
 }
--- src/main/java/com/example/bloompoem/repository/QnaRepository.java
+++ src/main/java/com/example/bloompoem/repository/QnaRepository.java
@@ -1,8 +1,14 @@
 package com.example.bloompoem.repository;
 
 import com.example.bloompoem.entity.QnaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface QnaRepository extends JpaRepository<QnaEntity, Integer> {
@@ -12,4 +18,15 @@ public interface QnaRepository extends JpaRepository<QnaEntity, Integer> {
     QnaEntity findByUserEmail(String userEmail);
 
     QnaEntity findByQnaTitleAndQnaContent(String qnaTitle, String qnaContent);
+
+    Page<QnaEntity> findAllByUserEmailOrderByQnaOrderAsc(String userEmail, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE qna q\n" +
+            "   SET q.qnaOrder = q.qnaOrder+ 1\n" +
+            " WHERE q.qnaGroup = :qna_group\n" +
+            "   AND q.qnaOrder > :qna_order")
+    int qnaOrderUpdate(int qna_group, int qna_order);
+
 }
