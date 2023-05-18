a3a04797d4fa60bf3c03bfa27b535b93dff9978a
Van <sung8881@naver.com>
2023-05-18 14:54:21 +0900

    TEST 컨트롤러 변경

--- src/main/java/com/example/bloompoem/controller/TestController.java
+++ src/main/java/com/example/bloompoem/controller/TestController.java
@@ -3,17 +3,11 @@ package com.example.bloompoem.controller;
 import com.example.bloompoem.repository.*;
 import com.example.bloompoem.service.PickUpService;
 import com.example.bloompoem.service.UserService;
-import com.example.bloompoem.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 
 @CrossOrigin(origins = "http://192.168.45.148:5500/")
 @Controller
@@ -49,26 +43,4 @@ public class TestController {
 
     @Value("#{environment['jwt.secret']}")
     private String secretKey;
-
-    @GetMapping(value = "/test")
-    public String testCall(HttpServletRequest request, @CookieValue(value = "Authorization") String token) {
-
-        System.out.println(token);
-        System.out.println(JwtUtil.getUserName(token, secretKey));
-        System.out.println(request.getSession());
-        return "test";
-    }
-
-    @GetMapping(value = "/test/list")
-    @ResponseBody
-    public ArrayList<String> testList(Model model) {
-        ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("cherryBlossom.jpg");
-        return arrayList;
-    }
-
-    @PostMapping(value = "/test/api")
-    public @ResponseBody ResponseEntity<?> test(String query) {
-        return ResponseEntity.ok().body(floristRepository.getFloristListAndReviewScoreAndFlowerColor(query));
-    }
 }
