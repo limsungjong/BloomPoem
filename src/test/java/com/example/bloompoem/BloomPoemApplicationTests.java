da87a2ed884abe20adc36cd6ab732f5d8dc80375
Van <sung8881@naver.com>
2023-05-18 14:53:00 +0900

    TEST 변경

--- src/test/java/com/example/bloompoem/BloomPoemApplicationTests.java
+++ src/test/java/com/example/bloompoem/BloomPoemApplicationTests.java
@@ -1,14 +1,7 @@
 package com.example.bloompoem;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BloomPoemApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
-
 }
